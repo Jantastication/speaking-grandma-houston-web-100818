4ffc3ee8e080eec281affd7388623b6937f0fe2c
Jantastication O <georginaoregx@gmail.com>
2018-09-30 04:47:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     'I LOVE YOU TOO PUMPKIN!'
-  elsif phrase == "WHERE ARE YOU GOING TODAY".upcase
-    "NO, NOT SINCE 1938!"
-  else phrase == "I love you Grandma".downcase
+  elsif phrase == "I love you Grandma".downcase
     "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938!"
   end
 end
 
